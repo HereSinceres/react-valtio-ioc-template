ccbf41257fbdbb7a162bcd21688a9844b6d69931
xukai12 <xukai12@baidu.com>
2023-04-12 14:59:13 +0800

    inti
    Change-Id: I918091ff5968f342e3e2e78a93952c4f7eea76f6

--- src/todo/todo.component.tsx
+++ src/todo/todo.component.tsx
@@ -7,19 +7,38 @@ import { TodoService } from './todo.service';
 type Props = {};
 
 const TodoViewComponent = (props: Props) => {
+  const todoService = useInstance(TodoService);
+  return (
+      <>
+          <div>{todoService.state.count}</div>
+      </>
+  );
+};
+const TodoViewNameComponent = (props: Props) => {
     const todoService = useInstance(TodoService);
     return (
         <>
-            <div>{todoService.state.count}</div>
+            <div>{todoService.state.name}</div>
+        </>
+    );
+};
+const TodoViewChangeNameComponent = (props: Props) => {
+    const todoService = useInstance(TodoService);
+    return (
+        <>
+            <div onClick={()=>{
+              todoService.setName(Math.random()+'');
+            }}> Change Name</div>
         </>
     );
 };
 export const TodoComponent = (props: Props) => {
-    const todoService = useInstance(TodoService);
     return (
         <>
             <TodoViewComponent />
             <TodoAction />
+            <TodoViewNameComponent />
+            <TodoViewChangeNameComponent />
         </>
     );
 };
